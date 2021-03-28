8a061c4e33af7efde090a387c87d175ae5de1cb0
jaimesilvarod <jaimesilvarod@hotmail.com>
2021-03-28 02:25:42 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/myconstraintlayout/MyConstraintLayout.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.myconstraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
